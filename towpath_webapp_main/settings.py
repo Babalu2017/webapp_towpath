adda405be786f500cd71c3af43ce93116b41dc6f
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-11-10 11:56:19 +0000

    added customer profile settings page with prepopulated form

--- towpath_webapp_main/settings.py
+++ towpath_webapp_main/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'my_shop',
     'marketplace',
     'django.contrib.gis',
+    'customers',
     
 ]
 
@@ -85,6 +86,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_user_profile',
                 'accounts.context_processors.get_google_api',
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amounts',
